5880c37f2da1a9b75d6c13bd6c2d4c2c5fe6d879
hexgeta <hexgeta@proton.me>
2025-05-20 09:50:23 +0100

    auto-commit

--- next.config.js
+++ next.config.js
@@ -29,7 +29,7 @@ const nextConfig = {
           },
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self' blob: data: https://unpkg.com; connect-src 'self' blob: https://unpkg.com; script-src 'self' 'unsafe-eval' 'wasm-unsafe-eval' 'inline-speculation-rules' blob: 'unsafe-inline' https://unpkg.com; worker-src 'self' blob: https://unpkg.com; child-src 'self' blob: https://unpkg.com; img-src 'self' blob: data: https://unpkg.com; media-src 'self' blob: data: https://unpkg.com; script-src-elem 'self' 'unsafe-inline' blob: https://unpkg.com;"
+            value: "default-src 'self' blob: data: https://unpkg.com https://plausible.io; connect-src 'self' blob: https://unpkg.com https://plausible.io; script-src 'self' 'unsafe-eval' 'wasm-unsafe-eval' 'inline-speculation-rules' blob: 'unsafe-inline' https://unpkg.com https://plausible.io; worker-src 'self' blob: https://unpkg.com; child-src 'self' blob: https://unpkg.com; img-src 'self' blob: data: https://unpkg.com; media-src 'self' blob: data: https://unpkg.com; script-src-elem 'self' 'unsafe-inline' blob: https://unpkg.com https://plausible.io;"
           }
         ],
       },
